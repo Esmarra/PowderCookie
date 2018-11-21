6c311206b1f535debabe96515c9bc5bcc9af7500
Esmarra <miguel.maranha@gmail.com>
2018-11-21 00:46:22 +0000

    Implemented Registo

--- base_dados.py
+++ base_dados.py
@@ -1,56 +1,78 @@
-## BD MAIN PROGRAM ##
-## 20/11
+# Program Name :	Music_for_all
+# Base Language:	English
+# Created by   :	Esmarra
+# Creation Date:	20/11/2018
+# Rework date entries:
+#
+# Program Objectives:
+# Observations:
+# Special Thanks:
 version ="alfa 0.01"
 
+# ==== Imports ===== #
 import os # to use cls
 import getpass # masks password
+import sys # exit clean
 
 from db_query import *
 
+# ==== Functions ==== #
+# Clears Console
 def cls(): os.system("CLS")
 
+# User Register
 def register():
-    print("registering")
+	cls();
+	print(" ==== User Register ==== ")
+	name=input(">What's your Name:")
+	user=input(">Type Username:")
+	password=getpass.getpass(">Type Password:")
+	edit=input(">Are you an editor?(y/n):")
+	if edit=="y":
+		user_level="admin"
+	register_user(name,user,password,edit)
 
+# User Login
 def login():
-    print("\nUser Login")
-    user=input(">Type Username:")
-    password=getpass.getpass(">Type Password:")
-    if(get_user(user,password)==True):
-        print("User Validaded, Welcome %s"%user)
-    else:
-        print("Invalid User...")
-        reg=input(">Register now?(y/n):")
-        if reg=="y":
-            register()
+	cls();
+	print(" ==== User Login ==== ")
+	user=input(">Type Username:")
+	password=getpass.getpass(">Type Password:")
+	if(get_user(user,password,"l")==True):
+		print("User Validaded, Welcome %s"%user)
+	else:
+		print("Invalid User...")
+		reg=input(">Register now?(y/n):")
+		if reg=="y":
+			register()
 
 # Pre Clean Up
 cls();
-print('==== Version: %s ===='%version)
+print(" ==== Version: %s ==== "%version)
 ans=True
 while ans:
-    print ("""
-    1.Login
-    2.Register
-    3.Add Music
-    4.Open Music
-    """)
-    ans=input(">Please select an option: ")
-    if ans=="1":
-        cls();
-        login();
-    elif ans=="2":
-        cls();
-        print("\n Student Deleted") 
-    elif ans=="3":
-        cls();
-        print("\n Student Record Found") 
-    elif ans=="4":
-        cls();
-        print("\n Goodbye") 
-    elif ans !="":
-        cls();
-        print("\n Not Valid Choice Try again")
+	print ("""
+		1.Login
+		2.Register
+		3.Add Music
+		4.Exit
+		""")
+	ans=input(">Please select an option: ")
+	if ans=="1":
+		login();
+	elif ans=="2":
+		register();
+		#Call Register
+	elif ans=="3":
+		cls();
+		# Call Add_Music
+	elif ans=="4":
+		cls();
+		print("Goodbye")
+		sys.exit()
+	elif ans !="":
+		cls();
+		print("\n Not Valid Choice Try again")
 
 
 
--- db_query.py
+++ db_query.py
@@ -1,4 +1,6 @@
 import pymysql
+import time
+from time import localtime, strftime
 
 #==== DB Setings ====#
 db_ip = "localhost" #"192.168.2.102"
@@ -7,8 +9,8 @@ db_pwd = ""
 db_name = "basedados"
 db_port = "" # 3306
 
-# Find all Users
-def get_user(user,password):
+# Get User From DB and compare with program login
+def get_user(user,password,type):
 	db = None
 	try:
 		db = pymysql.connect(host=db_ip,user=db_user,password=db_pwd,db=db_name,port=db_port)
@@ -17,9 +19,14 @@ def get_user(user,password):
 		cursor.execute(sql)
 		row = cursor.fetchone()
 		while row is not None:
-			# Validate Username and PWD here
-			if(user==row[0] and password==row[1]):
-				return True
+			#Type L (Login) User and PWD Validaion
+			if type=="l":
+				if(user==row[0] and password==row[1]):
+					return True
+			#Type R (Register) User only Validaion
+			if type =="r":
+				if(user==row[0]):
+					return True
 			row = cursor.fetchone()
 		cursor.close()
 
@@ -30,3 +37,25 @@ def get_user(user,password):
 			db.close()
 			
 	return False
+	
+def register_user(name,user,password,edit):
+	#Check for Duplicate Username
+	if get_user(user,password,"r") == True:
+		print("That Username is arleady Taken please try again")
+		return False
+	date=time.strftime("%Y-%m-%d", time.localtime())
+
+	try:
+		db = pymysql.connect(host=db_ip,user=db_user,password=db_pwd,db=db_name,port=db_port)
+		cursor = db.cursor()
+		sql = "INSERT INTO utilizador(name,username,password,user_level,data_de_registo\
+				)VALUES ('%s','%s','%s','%s','%s')"%(name,user,password,edit,date)
+		cursor.execute(sql)
+		db.commit()
+		
+	except pymysql.err.OperationalError:
+		print("db error, continue")
+	finally:
+		if db is not None:
+			db.close()
+	return True
